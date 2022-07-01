de3d9d9835b86b59f4d3196913260c31c3775bda
clarionke <104459747+clarionke@users.noreply.github.com>
2022-07-02 03:01:26 +0800

    Assigning random numbers to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,21 +5,22 @@
 /* more headers goes there */
 
 /**
-*main - assign a random number to the variable n each time it is executed based on condition
+*main - assign a random number to the variable n each time it is
+*executed based on condition
 *
 *Return : always 0
 */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-        printf("%d is positive\n", n);
-    if (n == 0)
-        printf("%d is zero\n", n);
-    if (n < 0)
-        printf("%d is negative\n", n);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+    printf("%d is positive\n", n);
+if (n == 0)
+    printf("%d is zero\n", n);
+if (n < 0)
+    printf("%d is negative\n", n);
     return (0);
 }
