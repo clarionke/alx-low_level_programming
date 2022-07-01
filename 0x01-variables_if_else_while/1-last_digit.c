eddc29577ca0013b75a4c6407367e03b9399a9ae
clarionke <104459747+clarionke@users.noreply.github.com>
2022-07-02 04:03:29 +0800

    Assigning random numbers to a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	printf("Last digit of %d is ", n);
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("%d and is greater than 5\n",n % 10);
 	}
 	if (n == 0)
 	{
-		printf("%d and is 0\n", n);
+		printf("%d and is 0\n", n % 10);
 	}
 	if (n < 6 && n !=0)
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	}
 	return (0);
 }
