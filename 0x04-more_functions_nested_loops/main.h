4bcd04dd30b3d28a793674382aa5e14e3a9f34f1
Joseph <jemijay028@gmail.com>
2022-07-07 22:01:24 -0700

    checking if a character is in uppercase

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Joseph N. Mwangi
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+
+#endif
