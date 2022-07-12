4d765242d2037f4da9e406d46bfc381e548fc3fc
Joseph <jemijay028@gmail.com>
2022-07-12 03:03:08 -0700

    adding task 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Joseph N. Mwangi
+ * Desc: Header file containing prototypes for all functions
+ * 	used in the 0x05-pointers_arrays_strings
+ */
+
+void reset_to_98(int *n);
+
+#endif
