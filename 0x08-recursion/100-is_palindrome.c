5d04ce093d7449f3fe0325354f0279c8036ca138
JOSEPH MWANGI <104459747+clarionke@users.noreply.github.com>
2022-07-20 19:16:24 +0800

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,66 +1,47 @@
 #include "main.h"
 
 /**
- * test - tests if wildcard is a proper match
- * @s5: string to compare
- * @s6: wildcard match to compare
- * @t5: first spot that was checked
- * @t6: first spot in wildcard comparison
+ * _strlen - return length of string
+ * @str: string to check
  *
- * Return: spot that matches or NULL
+ * Return: length of str
  */
-char *test(char *s5, char *s6, char *t5, char *t6)
+int _strlen(char *str)
 {
-	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
-		return (t5);
-	else if (*s5 != *s6)
-		return (wildcard(s5, t6));
-	else
-		return (test(s5 + 1, s6 + 1, t5, t6));
-}
-/**
- * wildcard - checks for the wildcards
- * @s3: string to compare to
- * @s4: string to check
- *
- * Return: pointer to spot in s3 that matches s4
- */
-char *wildcard(char *s3, char *s4)
-{
-	if (*s3 != *s4)
-	{
-		if (*s3 == '\0')
-			return (0);
-		return (wildcard(s3 + 1, s4));
-	}
-	return (test(s3 + 1, s4 + 1, s3, s4));
-}
-
-/**
- * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string, may contain wildcard *
- *
- * Return: 1 if match, 0 if not
- */
-int wildcmp(char *s1, char *s2)
-{
-	char *p;
-
-	if (*s2 == '*' && *(s2 + 1) == '\0')
-		return (1);
-	else if (*s2 == '*' && *(s2 + 1) == '*')
-		return (wildcmp(s1, s2 + 1));
-	else if (*s2 == '*')
-	{
-		p = wildcard(s1, s2 + 1);
-		if (p == 0)
-			return (0);
-		return (wildcmp(p + 1, s2 + 2));
-	}
-	else if (*s1 != *s2)
+	if (*str == '\0')
 		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
+	else
+		return (1 + _strlen(str + 1));
+}
+
+/**
+ * check_palindrome - checks to see if a string is a palindrome
+ * @l: left hand index
+ * @r: right hand index
+ * @p: possible palindrome
+ *
+ * Return: 1 if palindrome 0 if not
+ */
+int check_palindrome(int l, int r, char *p)
+{
+	if (l >= r)
 		return (1);
-	return (wildcmp(s1 + 1, s2 + 1));
+	else if (p[l] != p[r])
+		return (0);
+	else
+		return (check_palindrome(l + 1, r - 1, p));
+}
+
+/**
+ * is_palindrome - states if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = _strlen(s) - 1;
+	return (check_palindrome(0, i, s));
 }
