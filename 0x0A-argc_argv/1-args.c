a7b3f3adbab4573ceaad104bcdd49308736559fd
Joseph <jemijay028@gmail.com>
2022-07-22 04:28:50 -0700

    Adding 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,13 @@
 /**
  * main - prints the number of arguments passed into it
  *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
+ * @argc: number of arguments passed
+ * @argv: Arguments passed here
  * Return: Always 0 (success)
  */
 int main( int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
