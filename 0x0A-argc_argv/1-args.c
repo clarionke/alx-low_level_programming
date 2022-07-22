c2665255ad5ae290f7b9cddaf8a64bb47c36c8d5
Joseph <jemijay028@gmail.com>
2022-07-22 03:36:22 -0700

    Adding 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (success)
+ */
+int main( int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc -1);
+	return (0)
+}
